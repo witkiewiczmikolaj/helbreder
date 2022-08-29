6898ab5e185c1598d29b65871b0d29ea1e6a69d1
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2022-08-29 18:52:58 +0200

    basic auth

--- core/app.py
+++ core/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,json
+from basic_auth import *
 
 helbreder = Flask(__name__)
 
@@ -7,10 +8,11 @@ def static_main():
     return '<h1>&#128137;</h1>'
 
 @helbreder.route('/api',methods=['POST'])
+@auth.login_required
 def api_base():
     data = request.json
     print(data)
     return "Roger that!"
 
 if __name__ == "__main__":
-    helbreder.run(debug=True)
+    helbreder.run(debug=True, port=8080)
---
+++ core/basic_auth.py
@@ -0,0 +1,17 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_httpauth import HTTPBasicAuth
+
+user = 'admin'
+password = 'admin'
+
+users = {
+    user: generate_password_hash(password)
+}
+
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users:
+        return check_password_hash(users.get(username), password)
+    return False
