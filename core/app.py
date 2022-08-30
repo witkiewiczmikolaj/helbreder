23e028e213dec505f823a08844947a128c79f213
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2022-08-30 22:26:17 +0200

    smart func and logs

--- core/app.py
+++ core/app.py
@@ -1,6 +1,6 @@
+import datetime
 from flask import Flask,request,json
 from basic_auth import *
-
 from modules.kubernetes import *
 
 helbreder = Flask(__name__)
@@ -14,22 +14,22 @@ def static_main():
 def api_base():
     data = request.json
     print(data)
-    return "Roger that!"
+    return "Hi!"
 
 @helbreder.route('/api/k8s',methods=['POST'])
 @auth.login_required
 def k8s():
     data = request.get_json()
+
     ns = data["namespace"]
     action = data["action"]
     t_name = data["target_name"]
-    t_type = data["target_type"]
+    t_kind = data["target_kind"]
 
-    if action == "kill":
-        if t_type == "pod":
-            kill_pod(ns,t_name)
+    func = str(action + '_' + t_kind)
+    eval(func)(ns, t_name)
 
-    return f"Roger that!"
+    return f"[{datetime.datetime.now()}] action: {func} on {t_kind}/{t_name}\n"
 
 if __name__ == "__main__":
     helbreder.run(debug=True)
