97613184ae40d3ca2a5e4472fb7b4fb8c6fb8e73
witkiewiczmikolaj <witkiewiczmikolaj@gmail.com>
2022-09-19 12:28:07 +0200

    Improving post.py v2

---
+++ core/app/templates/action_target.py
@@ -0,0 +1,30 @@
+from flask import request
+from templates.modules_fcn import *
+from templates.actions_fcn import *
+from templates.targets_fcn import *
+from templates.post import button_clicked
+
+def action_target():
+    global button_clicked
+    code = 'Waiting for inputs'
+    rq = request.form.get
+
+    for module in range (len(modules())):
+            if rq(f'{modules()[module].title()}') == f'{modules()[module].title()}':
+                action = actions_buttonized(actions(modules()[module]))
+                target = targets_buttonized(targets(modules()[module]))
+                button_clicked[0] = rq(f'{modules()[module].title()}')
+                
+            for act in range (len(actions(modules()[module]))):
+                if rq(f'{actions(modules()[module])[act].title()}') == f'{actions(modules()[module])[act].title()}':
+                    action = actions_buttonized(actions(modules()[module]))
+                    target = targets_buttonized(targets(modules()[module]))
+                    button_clicked[1] = rq(f'{actions(modules()[module])[act].title()}')
+            
+            for targ in range (len(targets(modules()[module]))):
+                if rq(f'{targets(modules()[module])[targ].title()}') == f'{targets(modules()[module])[targ].title()}':
+                    action = actions_buttonized(actions(modules()[module]))
+                    target = targets_buttonized(targets(modules()[module]))
+                    button_clicked[2] = rq(f'{targets(modules()[module])[targ].title()}')
+
+    return action, target, code, button_clicked
--- core/app/templates/post.py
+++ core/app/templates/post.py
@@ -5,42 +5,32 @@ from templates.targets_fcn import *
 from templates.curl_to_code import *
 from templates.user_pass import *
 from templates.languages import *
+from templates.action_target import *
 
 button_clicked = ["*module*", "*action*", "*target*", "*username*", "*password*"]
 
 def collect_data():
     global button_clicked
     languages = open_lang()
+    rq = request.form.get
 
     for module in range (len(modules())):
-        
-        if request.form.get(f'{modules()[module].title()}') == f'{modules()[module].title()}':
-            action = actions_buttonized(actions(modules()[module]))
-            target = targets_buttonized(targets(modules()[module]))
-            button_clicked[0] = request.form.get(f'{modules()[module].title()}')
-            code = 'Waiting for inputs'
-            
+        if rq(f'{modules()[module].title()}') == f'{modules()[module].title()}':
+            action, target, code, button_clicked = action_target()
+
         for act in range (len(actions(modules()[module]))):
-            
-            if request.form.get(f'{actions(modules()[module])[act].title()}') == f'{actions(modules()[module])[act].title()}':
-                action = actions_buttonized(actions(modules()[module]))
-                target = targets_buttonized(targets(modules()[module]))
-                button_clicked[1] = request.form.get(f'{actions(modules()[module])[act].title()}')
-                code = 'Waiting for inputs'
+            if rq(f'{actions(modules()[module])[act].title()}') == f'{actions(modules()[module])[act].title()}':
+                action, target, code, button_clicked = action_target()
         
         for targ in range (len(targets(modules()[module]))):
-            
-            if request.form.get(f'{targets(modules()[module])[targ].title()}') == f'{targets(modules()[module])[targ].title()}':
-                action = actions_buttonized(actions(modules()[module]))
-                target = targets_buttonized(targets(modules()[module]))
-                button_clicked[2] = request.form.get(f'{targets(modules()[module])[targ].title()}')
-                code = 'Waiting for inputs'
+            if rq(f'{targets(modules()[module])[targ].title()}') == f'{targets(modules()[module])[targ].title()}':
+                action, target, code, button_clicked = action_target()
     
-    if request.form.get("Submit") == "Submit":
+    if rq("Submit") == "Submit":
         action, target, code, button_clicked = user_pass()
 
     for lang in languages:
-        if request.form.get(lang) == lang:
+        if rq(lang) == lang:
             action, target, code = lang_gen()
 
     return action, target, code
