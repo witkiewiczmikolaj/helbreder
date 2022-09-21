30e8804dfc08cce978fe56164d0ad3726ccc23d8
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2022-09-21 20:44:08 +0200

    helbreder:alpha_0.3.6

--- core/app/templates/log_collector.py
+++ core/app/templates/log_collector.py
@@ -5,4 +5,4 @@ def save_api_request(user_name, module, data):
     cur.execute(f'''INSERT INTO LOGS(TIME_STAMP, USER, MODULE, DATA) VALUES(current_timestamp, {user_name}, '{module}', '{data}')''')
     c.commit()
 
-create_table(table_name='LOGS', columns={"TIME_STAMP": "TIMESTAMP", "USER": "TEXT", "MODULE": "TEXT", "DATA": "JSONB"})
+create_table(table_name='LOGS', columns={"TIME_STAMP": "TIMESTAMP", "USER_NAME": "TEXT", "MODULE": "TEXT", "DATA": "JSONB"})
