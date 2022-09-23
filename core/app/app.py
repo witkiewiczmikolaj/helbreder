7099c2798385d5ae7f79220181d339e027db3a77
witkiewiczmikolaj <witkiewiczmikolaj@gmail.com>
2022-09-23 14:51:06 +0200

    Authorization added

--- core/app/app.py
+++ core/app/app.py
@@ -25,11 +25,12 @@ def static_main():
     return render_template('index.html', module = module, action = action, target = target, button_clicked = button_clicked, languages = languages)
 
 @helbreder.route('/code', methods=['GET'])
+@auth.login_required
 def code_outcome():
     try:
         code = lang_gen(request.args.get('code'))
-    except AttributeError:
-        abort(403)
+    except:
+        abort(500)
     return render_template('code.html', code = code)
 
 @helbreder.route('/api',methods=['POST'])
--- core/app/templates/psql.py
+++ core/app/templates/psql.py
@@ -5,6 +5,6 @@ psql_user = os.environ.get('POSTGRES_USER')
 psql_pass = os.environ.get('POSTGRES_PASSWORD') 
 psql_ip = os.environ.get('POSTGRES_IP') 
 psql_port = os.environ.get('POSTGRES_PORT') 
-psql_db_helbreder = 'helbreder'
-c = psycopg2.connect(database=psql_db_helbreder, user=psql_user, password=psql_pass, host=psql_ip, port=psql_port)
+psql_db = os.environ.get('POSTGRES_DB')
+c = psycopg2.connect(database=psql_db, user=psql_user, password=psql_pass, host=psql_ip, port=psql_port)
 cur = c.cursor()
