ce365ba01724ec3bcafcd4278d0b6025f758d5df
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2022-09-26 19:52:12 +0200

    psql envs

--- core/app/modules/postgresql.py
+++ core/app/modules/postgresql.py
@@ -2,10 +2,10 @@ import psycopg2
 import os
 
 def psql_connect_(psql_db):
-    psql_user = os.environ.get('POSTGRES_USER')
-    psql_pass = os.environ.get('POSTGRES_PASSWORD')
-    psql_ip = os.environ.get('POSTGRES_IP')
-    psql_port = os.environ.get('POSTGRES_PORT')
+    psql_user = os.environ.get('MODULE_POSTGRES_USER')
+    psql_pass = os.environ.get('MODULE_POSTGRES_PASSWORD')
+    psql_ip = os.environ.get('MODULE_POSTGRES_IP')
+    psql_port = os.environ.get('MODULE_POSTGRES_PORT')
 
     c = psycopg2.connect(database=psql_db, user=psql_user, password=psql_pass, host=psql_ip, port=psql_port)
     cur = c.cursor()
