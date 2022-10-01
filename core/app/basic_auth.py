381d69bc03bf554dc0d24a4dbc8f83e7473bfa74
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2022-10-01 10:36:57 +0200

    helbreder:alpha_0.5.0

--- core/app/basic_auth.py
+++ core/app/basic_auth.py
@@ -4,18 +4,25 @@ from flask_httpauth import HTTPBasicAuth
 
 user = os.environ.get('AUTH_USER')
 password = os.environ.get('AUTH_PASSWORD')
-# user_code = os.environ.get('AUTH_USER_CODE')
-# password_code = os.environ.get('AUTH_PASSWORD_CODE')
-
 users = {
     user: generate_password_hash(password),
-    # user_code: generate_password_hash(password_code)
+}
+
+user_code = os.environ.get('AUTH_USER_CODE')
+password_code = os.environ.get('AUTH_PASSWORD_CODE')
+code_users = {
+    user_code: generate_password_hash(password_code)
 }
 
 auth = HTTPBasicAuth()
+code_auth = HTTPBasicAuth()
 
 @auth.verify_password
 def verify_password(username, password):
     if username in users:
         return check_password_hash(users.get(username), password)
     return False
+
+@code_auth.verify_password
+def verify_password(username, password):
+    return check_password_hash(users.get(username), password)
