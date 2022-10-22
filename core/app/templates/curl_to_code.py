e4c014305f66aeecf4dc9aa48f39ae5e9a96aead
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2022-10-22 19:01:56 +0200

    helbreder:alpha_0.7.21

--- core/app/templates/curl_to_code.py
+++ core/app/templates/curl_to_code.py
@@ -1,8 +1,10 @@
 import subprocess
-import sys
+
+def command_maker(command):
+    return command.split(' ')
 
 def curl_to_code(command, language):
-    print(command, file=sys.stderr)
+    command = command_maker(command)
     curl_converted = ['curlconverter', '--language', language, command]
     curl_converted = subprocess.check_output(curl_converted).decode("utf-8") 
     return curl_converted
