795e2c659fb00e8d25744cbd0bbe2f4be117d4fd
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2022-10-23 15:00:01 +0200

    helbreder:alpha_0.7.255

--- core/app/templates/languages.py
+++ core/app/templates/languages.py
@@ -12,13 +12,6 @@ def lang_gen(lang):
         data['target_name'] = button_clicked[5]
         data['target_kind'] = button_clicked[2]
         data_json = json.dumps(data)
-    elif button_clicked[0] == "Server":
-        data['action'] = button_clicked[1]
-        data['user'] = button_clicked[6]
-        data['target_kind'] = button_clicked[2]
-        data['IP'] = button_clicked[7]
-        data['resource_type'] = button_clicked[8]
-        data_json = json.dumps(data)
     else:
         data['action'] = button_clicked[1]
         data['target_name'] = button_clicked[5]
@@ -47,6 +40,6 @@ def lang_buttonized():
 
     langs = []
     for lang in languages:
-        langs.append('<input type="submit" name="Submit" value="Submit" class="language_logo" id="' + lang.lower() + '_butt" value="' + lang + '" img src="../static/images/' + lang.lower() + '.png"/>')
+        langs.append('<button class="language_logo" id="' + lang.lower() + '_butt" type="button" value="' + lang + '"><img src="../static/images/' + lang.lower() + '.png"/></button>')
     langs = ' '.join(langs)    
     return langs
